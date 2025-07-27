28f3ff2c71cb034b59c527e6bfc4b660674f5d75
MaccSob <71sobmaciej@gmail.com>
2025-07-28 00:16:58 +0200

    rr

--- joffy/vite.config.js
+++ joffy/vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
-import withMT from '@material-tailwind/react/utils/withMT';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
+  plugins: [react(),
     tailwindcss()
-  ]
+  ],
+  
 })
