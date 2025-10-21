d23495e0800c2e6349449c75e38512edba5ec635
MaccSob <71sobmaciej@gmail.com>
2025-10-21 23:59:21 +0200

    underline nav, some bs on about

--- joffy/src/components/About.jsx
+++ joffy/src/components/About.jsx
@@ -7,7 +7,14 @@ return (
 
 
     <>
-    <h1>Who are we?</h1>
+    <h2>Who are we?</h2>
+    <section>
+
+    <p>Company based in Transylvania</p>
+    <p>CEO is Dracula.</p>
+    <p>Developers of the App are also vampires.</p>
+
+    </section>
     
     </>
 
