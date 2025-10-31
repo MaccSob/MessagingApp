24d3dd8b0bc65ed95b0145cf45d6da80bcdb4b9e
MaccSob <71sobmaciej@gmail.com>
2025-10-31 23:59:22 +0100

    f

--- joffy/src/components/Sidebar.jsx
+++ joffy/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import '../Sidebar.css';
-
+import { FaMessage, FaUser } from "react-icons/fa6";
 import { Link } from 'react-router';
 
 const Sidebar = () => {
@@ -7,17 +7,28 @@ const Sidebar = () => {
 
     return (
 <>
- <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
  <div className="fixed top-0 left-0 h-screen w-24 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-         <ul>
-         <li> <Link to={'/messages'}> Messages</Link></li>
-         <li> <Link to={'/profile'}>Profile   </Link></li>
+        <Link to={'/messages'}> Messages</Link>
+         <Link to={'/profile'}>Profile   </Link>
          <button>Log out</button>
-        </ul>
+         
+
+         <SidebarIcon icon={<FaMessage size="32"/>}/>
+                  <SidebarIcon icon={<FaUser size="28"/> }/>
+
 </div>
+
+
 
 </>
         
     )
 }
+
+const SidebarIcon = ({icon}) => (
+    <div className="sidebar-icon">
+
+        {icon}
+    </div>
+)
 export default Sidebar;
