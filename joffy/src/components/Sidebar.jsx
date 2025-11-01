2d80cb78b8255585496175bdf8f8aa46624aa8fd
MaccSob <71sobmaciej@gmail.com>
2025-11-01 23:47:17 +0100

    swapping to vcss

--- joffy/src/components/Sidebar.jsx
+++ joffy/src/components/Sidebar.jsx
@@ -1,5 +1,4 @@
 import '../Sidebar.css';
-import { FaMessage, FaUser } from "react-icons/fa6";
 import { Link } from 'react-router';
 
 const Sidebar = () => {
@@ -7,14 +6,10 @@ const Sidebar = () => {
 
     return (
 <>
- <div className="fixed top-0 left-0 h-screen w-24 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
+ <div className="fixed top-0 left-0 h-screen w-32 m-0 flex flex-col bg-gray-900 shadow-lg">
         <Link to={'/messages'}> Messages</Link>
          <Link to={'/profile'}>Profile   </Link>
          <button>Log out</button>
-         
-
-         <SidebarIcon icon={<FaMessage size="32"/>}/>
-                  <SidebarIcon icon={<FaUser size="28"/> }/>
 
 </div>
 
@@ -25,10 +20,4 @@ const Sidebar = () => {
     )
 }
 
-const SidebarIcon = ({icon}) => (
-    <div className="sidebar-icon">
-
-        {icon}
-    </div>
-)
 export default Sidebar;
